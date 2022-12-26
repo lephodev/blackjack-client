66c4460c6a9b5c9ac087604246f2dc3096f9b4be
ashrafwbst <er.ashrafkamal@gmail.com>
2022-12-26 16:36:28 +0530

    change server path

--- src/config/contants.js
+++ src/config/contants.js
@@ -1,7 +1,7 @@
 const env = {
   dev: {
     landingServerUrl: 'https://api.scrooge.casino/v1',
-    serverUrl: 'https://blackjack-api.scrooge.casino',
+    serverUrl: 'http://localhost:3001',
     landingClient: 'http://localhost:3000',
   },
   production: {
