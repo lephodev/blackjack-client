e05dd9bb41b309c63d66f4ae5ee9a3213e9c7f38
ashrafwbst <er.ashrafkamal@gmail.com>
2023-01-23 18:42:46 +0530

    tourPopup and button click event updated

--- src/components/Game/tourpopup.jsx
+++ src/components/Game/tourpopup.jsx
@@ -1,27 +1,28 @@
 import React from "react";
-import {Modal} from "react-bootstrap"
+import { Modal } from "react-bootstrap"
 import tourimg from "../../imgs/blackjack/tour-img.png"
-const TourPopup = ({isTourOpen,handleClose}) => {
+const TourPopup = ({ isTourOpen, handleClose }) => {
 
-  return (
-    <Modal show={isTourOpen} onHide={handleClose} centered className="tour-popup">
-        <Modal.Header closeButton>
-        <Modal.Title>How to play</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-            <div className="tour-content">
+    return (
+        <Modal show={isTourOpen} onHide={handleClose} centered className="tour-popup">
+            <Modal.Header closeButton>
+                <Modal.Title>How to play</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div className="tour-content">
 
-                <div className="tour-image">
-                    <img src={tourimg} alt="" />
+                    <div className="tour-image">
+                        <img src={tourimg} alt="" />
+                    </div>
+
+                    <div className="tour-info">
+                        <p>Please click on the chips to place your bet and click Bet Now button.</p>
+                        <button className="tour-info-btn" type='button' onClick={() => { localStorage.setItem("DontShowAgain",true); handleClose() }}>Don't show me again</button>
+                    </div>
                 </div>
-
-                <div className="tour-info">
-                    <p>Please click on the chips to place your bet and click Bet Now button.</p>
-                </div>
-            </div>
-        </Modal.Body>
-    </Modal>
-  );
+            </Modal.Body>
+        </Modal>
+    );
 };
 
 export default TourPopup;
