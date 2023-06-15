e0761171f2754c2b7eadd6a2523ef6cf1504a1f1
jivan-deep <jivan@websultanate.com>
2023-06-15 12:38:59 +0530

    added permission policy

--- src/utils/axios.config.js
+++ src/utils/axios.config.js
@@ -8,24 +8,35 @@ const getAuthorizationHeader = () => {
 export const userInstance = () =>
   axios.create({
     baseURL: `${CONSTANTS.landingServerUrl}/users`,
-    headers: { Authorization: getAuthorizationHeader() },
+    headers: { 
+      Authorization: getAuthorizationHeader(),
+      "Permissions-Policy": "geolocation=*",
+    },
+
   });
 
 export const authInstance = () =>
   axios.create({
     baseURL: `${CONSTANTS.landingServerUrl}/auth`,
-    headers: { Authorization: getAuthorizationHeader() },
+    headers: { 
+      Authorization: getAuthorizationHeader(),
+      "Permissions-Policy": "geolocation=*",
+    },
   });
 
 export const blackjackInstance = () =>
   axios.create({
     baseURL: `${CONSTANTS.serverUrl}`,
-    headers: { Authorization: getAuthorizationHeader() },
+    headers: { 
+      Authorization: getAuthorizationHeader(),
+      "Permissions-Policy": "geolocation=*",
+    },
   });
   export const ticketTotokenInstance = () =>
   axios.create({
     baseURL: `${CONSTANTS.marketServer}/api`,
     headers: {
       Authorization: getCookie('token') ? `Bearer ${getCookie('token')}` : '',
+      "Permissions-Policy": "geolocation=*",
     },
   });
